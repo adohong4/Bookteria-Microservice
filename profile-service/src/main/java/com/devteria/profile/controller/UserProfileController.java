46b264bd7c2240f32f715281a0fdc17a86cd8782
adohong4 <adohong4@gmail.com>
2024-08-26 15:19:47 +0700

    Authentication for Api Gateway

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -9,19 +9,21 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserProfileController {
     UserProfileService userProfileService;
 
-    @PostMapping("/users")
-    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request){
-        return userProfileService.createProfile(request);
+    @GetMapping("/users/{profileId}")
+    UserProfileResponse getProfile(@PathVariable String profileId) {
+        return userProfileService.getProfile(profileId);
     }
 
-    @GetMapping("/{profileId}")
-    UserProfileResponse getProfile(@PathVariable String profileId){
-        return userProfileService.getProfile(profileId);
+    @GetMapping("/users")
+    List<UserProfileResponse> getAllProfiles() {
+        return userProfileService.getAllProfiles();
     }
 }
