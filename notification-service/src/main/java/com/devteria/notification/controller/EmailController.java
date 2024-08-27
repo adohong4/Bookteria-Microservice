f9c56b2925d0e30c200653cf3478ba5e5f83837a
adohong4 <adohong4@gmail.com>
2024-08-27 12:49:32 +0700

    send message in kafka

--- notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
+++ notification-service/src/main/java/com/devteria/notification/controller/EmailController.java
@@ -7,6 +7,9 @@ import com.devteria.notification.service.EmailService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class EmailController {
+    private static final Logger log = LoggerFactory.getLogger(EmailController.class);
     EmailService emailService;
 
     @PostMapping("/email/send")
@@ -22,5 +26,10 @@ public class EmailController {
         return ApiResponse.<EmailResponse>builder()
                 .result(emailService.sendEmail(request))
                 .build();
+    }
+
+    @KafkaListener(topics = "onboard-successful")
+    public void listen(String message){
+        log.info("Message received: {}", message);
     }
 }
